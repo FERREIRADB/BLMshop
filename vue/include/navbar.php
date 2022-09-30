1277c57b975386eee793cae70e2a5922d94ead01
lucas-chvnn <lucas.chvnn@eduge.ch>
2022-09-30 09:18:16 +0200

    Update navbar.php

--- vue/include/navbar.php
+++ vue/include/navbar.php
@@ -14,7 +14,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Produit</a>
+          <a class="nav-link" href="./produits.php">Produit</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">A propos de nous</a>
