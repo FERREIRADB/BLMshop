9ff4e9d6dda16bcf1f2774803672ee860d9f9634
lucas-chvnn <lucas.chvnn@eduge.ch>
2022-09-30 09:15:07 +0200

    page produits

---
+++ vue/produits.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produits</title>
+    <link rel="stylesheet" src="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <?php include "include/navbar.php"; ?>
+    <h1>ayaaaaaaa</h1>
+</body>
+</html>
