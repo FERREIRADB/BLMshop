e330a094639d3cf42929075a2d6ea172a09b579b
lucas-chvnn <lucas.chvnn@eduge.ch>
2022-10-05 15:57:00 +0200

    aya

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,4 +1,5 @@
 <?php require_once '../modele/config.php'; ?>
+<?php require_once '../modele/fonctions.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,12 +16,18 @@
     <?php
     include "include/navbar.php";
     ?>
+    <ol>
+        <div>
+            <?php categorie(); ?>
+        </div>
+    </ol>
     <header>
         <!-- The HTML5 video element that will create the background video on the header -->
         <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" style="width:1800px; height:750px; margin-top:100px;">
             <source src="../img/Venom F82 BMW M4 on TE37s [4K].mp4" type="video/mp4">
         </video>
     </header>
+    
     <div style="text-align:center;">
         <h1>Bienvenu sur notre site de vente de pièces BMW</h1>
         <p>Fait par des passionné pour des passionné</p>
--- vue/produits.php
+++ vue/produits.php
@@ -11,7 +11,36 @@
     <title>Produits</title>
 </head>
 <body>
-    <?php include "include/navbar.php"; $request = "select * from produits ";?>
+<?php include "include/navbar.php"; $request = "select * from produits ";?>
+<div style="margin-left:50px;">
+            <a>Ordonner par :</a>
+            <a href="produits.php?order=asc">Ordre alphabétique</a>
+            <a href="produits.php?order=productType">Type de produit</a>
+            <a href="produits.php?order=priceplus">Le moins cher (vous êtes pauvres)</a>
+            <a href="produits.php?order=pricemoins">Le plus cher (vous êtes riches)</a>
+        </div>
+        </ul>
+
+        <?php
+        $request = "select * from produits ";
+
+        
+        if(isset($_GET['data'])){
+            $request .= "WHERE productType = '" . $_GET['data'] . "'";
+        }
+        if (isset($_GET['order']) && $_GET['order'] != "asc") {
+            if ($_GET['order'] == "priceplus") {
+                $request .= " ORDER BY price ASC";
+            } else if ($_GET['order'] == "pricemoins") {
+                $request .= " ORDER BY price DESC";
+            } else {
+                $request .= " ORDER BY " . $_GET['order'];
+            }
+        }
+        if (isset($_GET['order']) && $_GET['order'] == "asc") {
+            $request .= " ORDER BY name ASC";
+        }
+        ?>
     <article class="articleProduct" style="width: 100%; text-align: center;"><?= produitsDiv($request) ?></article>
 
 </body>
