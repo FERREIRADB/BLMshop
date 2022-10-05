e9852e29f0cdc04ace34b7fc3b9ba8b30524793b
FERREIRADB <61586342+FERREIRADB@users.noreply.github.com>
2022-10-05 09:36:13 +0200

    Update accueil.php

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,18 +1,24 @@
 <?php require_once '../modele/config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style> 
+</style>
 </head>
+
 <body>
     <?php
-        include "include/navbar.php";
-        include "include/carousel.php";
+    include "include/navbar.php";
+    include "include/carousel.php";
     ?>
+
     <h1>Bienvenu sur notre site de vente de pièces BMW</h1>
     <p>Fait par des passionné pour des passionné</p>
 </body>
+
 </html>
